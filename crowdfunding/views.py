8abbdb8f2c0312345feada546c4a6f18f11d5d87
rasoulazizi <rasoulazizi@outlook.com>
2026-02-05 22:06:30 +0330

    (crowdfunding): rewrite IsOwnerReadOnly permission in permission.py

--- crowdfunding/views.py
+++ crowdfunding/views.py
@@ -3,12 +3,12 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Campaign
 from .serializers import CampaignSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from .permissions import IsOwnerOrReadOnly
 
 class CampaignViewSet(ModelViewSet):
     queryset = Campaign.objects.all()
     serializer_class = CampaignSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
