fea647194db2cb1a72bd8cea51f9e39a2239af5e
rasoulazizi <rasoulazizi@outlook.com>
2026-02-05 21:26:30 +0330

    (crowdfunding): change user to owner | dead_line to deadline feilds

--- crowdfunding/models.py
+++ crowdfunding/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Campaign(models.Model):
-    User = models.ForeignKey(User, on_delete= models.CASCADE, related_name='campaigns')
+    owner = models.ForeignKey(User, on_delete= models.CASCADE, related_name='campaigns')
     title = models.CharField(max_length=220)
     description = models.TextField()
     target_amount = models.DecimalField(max_digits=10, decimal_places=2)
     current_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    dead_line = models.DateTimeField()
+    deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
