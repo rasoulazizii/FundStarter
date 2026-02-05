180a3feb22009fa4a28464eaea3b91223683700a
rasoulazizi <rasoulazizi@outlook.com>
2026-02-05 21:22:37 +0330

    (crowdfunding): create campaign model

--- crowdfunding/models.py
+++ crowdfunding/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Campaign(models.Model):
+    User = models.ForeignKey(User, on_delete= models.CASCADE, related_name='campaigns')
+    title = models.CharField(max_length=220)
+    description = models.TextField()
+    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    current_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    dead_line = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
