b18836b7a29ec9ce85345f46c7ce81d5c0050ffb
rasoulazizi <rasoulazizi@outlook.com>
2026-02-05 20:52:35 +0330

    (accounts):create user model(abstractuser) and user manager

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
+# user model
+AUTH_USER_MODEL = 'accounts.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
