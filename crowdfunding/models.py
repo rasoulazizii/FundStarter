0f9aa9f222a60a34f2f4d0e69acba0b007b38171
rasoulazizi <rasoulazizi@outlook.com>
2026-02-06 21:06:25 +0330

    (crowdfunding): create donation model

--- crowdfunding/models.py
+++ crowdfunding/models.py
@@ -15,4 +15,13 @@ class Campaign(models.Model):
     def __str__(self):
         return self.title
     
+class Donation(models.Model):
+    campaign = models.ForeignKey(Campaign, on_delete=models.PROTECT, related_name='donatins')
+    backer = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.backer} donated {self.amount} to {self.campaign}'
+    
+    
